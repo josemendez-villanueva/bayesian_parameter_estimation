3d453c7d62a5572cb671228d2dfe10858671d71b
Jose Mendez-Villanueva <joseph13.mv@gmail.com>
2021-07-07 11:23:27 -0700

    Network(Simple) implementation of sbi

--- optimization.py
+++ optimization.py
@@ -12,7 +12,11 @@ from sbi.inference.base import infer
 def simulator(params):
     params = np.asarray(params)
 
-    sim_single_cell.netParams.stimSourceParams['background']['noise'] = params
+    sim_single_cell.netParams.stimSourceParams['background']['noise'] = params[0]
+    sim_single_cell.netParams.connParams['P1 -> P2']['probability'] = params[1]
+    sim_single_cell.netParams.connParams['P1 -> P2']['weight'] = params[2]
+    sim_single_cell.netParams.connParams['P1 -> P2']['delay'] =params[3]
+
     """
     Way to simulate this without always running it with the displays?
      
@@ -34,21 +38,22 @@ def simulation_wrapper(params):
     return summstats
 
 #Ground Truth Parameters
-baseline_param = np.array([.8])
+baseline_param = np.array([.2, 0.2, 0.025, 2])
 baseline_simulator = simulator(baseline_param)
 observable_baseline_stats = torch.as_tensor(baseline_simulator['stats'])
 
 
 #Inference
 
-prior_min = np.array([0])
-prior_max = np.array([2])
+
+prior_min = np.array([0, 0.01, 0.001, 1])
+prior_max = np.array([2, 0.5, 0.1, 20])
 
 prior = utils.torchutils.BoxUniform(low=torch.as_tensor(prior_min), 
                                     high=torch.as_tensor(prior_max))
 
 posterior = infer(simulation_wrapper, prior, method='SNPE', 
-                  num_simulations=1000, num_workers=4)
+                  num_simulations=5, num_workers=4)
 
 
 samples = posterior.sample((10000,),
@@ -58,17 +63,18 @@ samples = posterior.sample((10000,),
 posterior_sample = posterior.sample((1,),
                                         x = observable_baseline_stats).numpy()
 
-
+#posterior prints a list within a list so when using it to simulate again, use the index 0 so that the above
+#can calibrate it right
 
 #Plot Observed and Posterior
+x = simulator(posterior_sample[0])
 
-fig = plt.figure(figsize=(12,10))
+fig = plt.figure(1, figsize=(12,10))
 
 gs = mpl.gridspec.GridSpec(2,1,height_ratios=[4,1])
 ax = plt.subplot(gs[0])
 
 #x is simulation of Posterior
-x = simulator(posterior_sample)
 t = baseline_simulator['time']
 
 plt.plot(t, baseline_simulator['traces'], '-r' ,lw=2, label='observation')
@@ -85,4 +91,3 @@ ax.legend(handles[::-1], labels[::-1], bbox_to_anchor=(1.3, 1),
 plt.legend()
 plt.show()
 
-print(posterior_sample)
--- sim_single_cell.py
+++ sim_single_cell.py
@@ -4,7 +4,38 @@ from netpyne.specs import Dict
 netParams = specs.NetParams()
 
 
+#Simple Network
+netParams.cellParams['PYR'] = {
+    'secs': {'soma':
+            {'geom': {'diam': 18.8, 'L': 18.8, 'Ra': 123.0},
+            'mechs': {'hh': {'gnabar': 0.12, 'gkbar': 0.036, 'gl': 0.003, 'el': -70}}}},
+            'dend':
+            {'geom': {'diam': 5.0, 'L': 150.0, 'Ra': 150.0, 'cm': 1},
+            'mechs': {'pas': {'g': 0.0000357, 'e': 0}}}}
+
+
+
+netParams.popParams['P1'] = {'cellType': 'PYR', 'numCells': 20, 'cellModel': 'HH'}
+netParams.popParams['P2'] = {'cellType': 'PYR', 'numCells': 20, 'cellModel': 'HH'}
+
+
+netParams.synMechParams['exc'] = {'mod': 'Exp2Syn', 'tau1': 0.1, 'tau2': 5.0, 'e': 0}
+
+netParams.stimSourceParams['background'] = {'type': 'NetStim', 'rate': 10, 'noise': 0} #This noise is just a dummy variable
+netParams.stimTargetParams['background->Pop'] = {'source': 'background', 'conds': {'cellType': 'PYR'}, 'weight': 0.03, 'delay': 10, 'synMech': 'exc'}
+
+netParams.connParams['P1 -> P2'] = {
+                                    'preConds': {'pop': 'P1'},
+                                    'postConds': {'pop': 'P2'},
+                                    'probability': 0.2,
+                                    'weight': 0.025,
+                                    'delay': 1,
+                                    'synMech': 'exc'
+	                                }
+
+
 # This will create the basic cell model that we will be using: Creating both the soma and a dendritic section
+"""
 netParams.cellParams['PYR'] = {
     'secs': {'soma':
             {'geom': {'diam': 18.8, 'L': 18.8, 'Ra': 123.0},
@@ -25,6 +56,9 @@ netParams.synMechParams['exc'] = {'mod': 'Exp2Syn', 'tau1': 0.1, 'tau2': 5.0, 'e
 
 netParams.stimSourceParams['background'] = {'type': 'NetStim', 'rate': 10} #This noise is just a dummy variable
 netParams.stimTargetParams['background->Pop'] = {'source': 'background', 'conds': {'cellType': 'PYR'}, 'weight': 0.03, 'delay': 10, 'synMech': 'exc'}
+
+
+"""
 
 cfg = specs.SimConfig()
 
